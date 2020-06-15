6690ce40abbe610055ba0c64cd9ef4bd4655c0f8
이완석 <k1nder@naver.com>
2020-06-16 05:55:17 +0900

    react structure design

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
-var router = express.Router();
+var mysql_odbc = require('../database/database_conn')();
+
+const router = express.Router();
+const connection = mysql_odbc.init();
 
 router.get('/', function(req, res){
 
--- server/server.js
+++ server/server.js
@@ -1,11 +1,30 @@
 const express = require('express');
+const mysql = require('mysql');
+
+const indexRouter = require('./routes/index');
+
 const app = express();
-const api = require('./routes/index');
-const cors = require('cors');
 
-app.use(cors());
-app.use(api);
+// interlink router
+app.use(indexRouter);
 
+/** 현재 실행한 파일의 이름과 Path*/
+//console.log('finaname : ' + __filename);
+
+/** 현재 실행한 파일의 Path */
+//console.log('dirname : ' + __dirname);
 
 const port = 3002;
+
+
+// 1. http 모듈을 이용한 웹서버
+// var http = require('http');
+// var server = http.createServer();
+// server.listen(port, host, 50000, function(){
+//    console.log('');
+// });
+
+// 2. express를 이용한 웹서버
+// const express = require('express');
+// const app = express();
 app.listen(port, ()=>console.log(`Listening on port ${port}`));
